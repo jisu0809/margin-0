a9542dc29e9ae2db990bd2ff151a5eb80e1a8b03
jisu <oceanjisu@yonsei.ac.kr>
2025-05-05 12:11:56 +0900

    모르겠다

--- main/backend.py
+++ main/backend.py
@@ -1,20 +1,54 @@
 from flask import Flask, render_template, request, session, redirect, url_for
+import sqlite3
+
 app = Flask(__name__)
 app.secret_key = 'fjaoidfj'
 
+
 @app.route('/')
 def index():
-        return redirect(url_for('login'))
+        return redirect(url_for('signup'))
 
-@app.route('/login', methods = ['GET','POST'])
-def login():
+@app.route('/signup', methods = ['GET','POST'])
+def signup():
         if request.method == 'POST':
                 session['username'] = request.form['username']
                 session['password'] = request.form['password']
                 session['password_match'] = request.form['password_match']
-                return redirect(url_for('mainpage_prof'))
+                session['major'] = request.form['major']
+                session['studentNUM'] = request.form['studentNUM']
+                session['grade'] = request.form['grade']
+
+                conn = sqlite3.connect('logindatabase.db')
+                cursor = conn.cursor()
+                insert_logininfo = "INSERT INTO signup_info_student(Username,Password,PasswordCHECK,Major,Grade,StudentNumber) VALUES (?,?,?,?,?,?)"
+                cursor.execute(insert_logininfo,(session['username'],session['password'],session['password_match'],
+                                                session['major'],session['grade'],session['studentNUM']))
+                conn.commit()
+                conn.close()
+
+                return redirect(url_for('login'))
         else:
-                return render_template("loginpage.html")
+                return render_template("signup.html")
+
+@app.route('/login', methods = ['GET','POST'])
+def signup():
+        if request.method == 'POST':
+                session['username'] = request.form['username']
+                session['password'] = request.form['password']
+
+                conn = sqlite3.connect('logindatabase.db')
+                cursor = conn.cursor()
+                select_logininfo = "INSERT INTO signup_info_student(Username,Password,PasswordCHECK,Major,Grade,StudentNumber) VALUES (?,?,?,?,?,?)"
+                cursor.execute(insert_logininfo,(session['username'],session['password'],session['password_match'],
+                                                session['major'],session['grade'],session['studentNUM']))
+                conn.commit()
+                conn.close()
+
+                return redirect(url_for('mainpage_prof.html'))
+        else:
+                return render_template("signup.html")
+
 
 @app.route('/mainpage_prof')
 def mainpage_prof():        
