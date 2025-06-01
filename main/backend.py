5a90d66944b7850be2d25689bf94579a8a5b7d9c
jisu <oceanjisu@yonsei.ac.kr>
2025-06-01 23:40:45 +0900

    로그인 signup 최종

--- main/backend.py
+++ main/backend.py
@@ -12,30 +12,51 @@ def index():
 @app.route('/signup', methods = ['GET','POST'])
 def signup():
         if request.method == 'POST':
+                session['porf_or_stu'] = request.form['prof_or_stu']
                 session['username'] = request.form['username']
                 session['password'] = request.form['password']
                 session['password_match'] = request.form['password_match']
                 session['major'] = request.form['major']
                 session['studentNUM'] = request.form['studentNUM']
                 session['grade'] = request.form['grade']
-
-                conn = sqlite3.connect('logindatabase.db')
-                cursor = conn.cursor()
-                cursor.execute('SELECT * FROM signup_info_student WHERE username = ?', (session['username'],))
-                existing_user = cursor.fetchone()
-                if existing_user: 
-                        conn.commit()
-                        conn.close()
-                        flash('이미 존재하는 사용자 이름입니다.')
-                        return render_template("signup.html")
-                else:
-                        insert_logininfo = "INSERT INTO signup_info_student(Username,Password,PasswordCHECK,Major,Grade,StudentNumber) VALUES (?,?,?,?,?,?)"
-                        cursor.execute(insert_logininfo,(session['username'],session['password'],session['password_match'],
+                
+                if session['prof_or_stu'] == 'student':
+                        conn = sqlite3.connect('logindatabase.db')
+                        cursor = conn.cursor()
+                        cursor.execute('SELECT * FROM signup_info_student WHERE username = ?', (session['username'],))
+                        existing_user = cursor.fetchone()
+                        if existing_user: 
+                                conn.commit()
+                                conn.close()
+                                flash('이미 존재하는 사용자 이름입니다.')
+                                return render_template("signup.html")
+                        else:
+                                insert_logininfo = "INSERT INTO signup_info_student(Username,Password,PasswordCHECK,Major,Grade,StudentNumber) VALUES (?,?,?,?,?,?)"
+                                cursor.execute(insert_logininfo,(session['username'],session['password'],session['password_match'],
                                                 session['major'],session['grade'],session['studentNUM']))
-                        conn.commit()
-                        conn.close()
-                        flash('성공적으로 회원가입 되었습니다.')
-                        return redirect(url_for('login'))
+                                conn.commit()
+                                conn.close()
+                                flash('성공적으로 회원가입 되었습니다.')
+                                return redirect(url_for('login'))
+                elif session['prof_or_stu'] == 'prof':
+                        conn = sqlite3.connect('logindatabase.db')
+                        cursor = conn.cursor()
+                        cursor.execute('SELECT * FROM signup_info_prof WHERE username = ?', (session['username'],))
+                        existing_user = cursor.fetchone()
+                
+                        if existing_user: 
+                                conn.commit()
+                                conn.close()
+                                flash('이미 존재하는 사용자 이름입니다.')
+                                return render_template("signup.html")
+                        else:
+                                insert_logininfo = "INSERT INTO signup_info_prof(Username,Password,Major,ProfNumber) VALUES (?,?,?,?)"
+                                cursor.execute(insert_logininfo,(session['username'],session['password'],
+                                                session['major'], session['profNUM']))
+                                conn.commit()
+                                conn.close()
+                                flash('성공적으로 회원가입 되었습니다.')
+                                return redirect(url_for('login'))
         else:
                 return render_template("signup.html")
 
@@ -47,21 +68,38 @@ def login():
                 session['password'] = request.form['password']
                 conn = sqlite3.connect('logindatabase.db')
                 cursor = conn.cursor()
-                cursor.execute("SELECT * FROM signup_info_student WHERE username = ? AND password = ?", session['username'], session['password'])
-                q = "SELECT password FROM users WHERE username = ?;"
-                cursor.execute(q, (session['username'],))
-                rows = cursor.fetchone()
-                print(rows)
-                print(rows[0])
+                if session['prof_or_stu'] == 'student':
+                        conn = sqlite3.connect('logindatabase.db')
+                        cursor = conn.cursor()
+                        cursor.execute('SELECT password FROM signup_info_student WHERE username = ?', (session['username'],))
+                        info = cursor.fetchone()
+                        if info: 
+                                stored_password = info[0]
+                        
+                                if (session['username'],) == stored_password:
+                                        return render_template("login.html")
+                        else: 
+                                return render_template("login.html")
+                elif session['prof_or_stu'] == 'prof':
+                        conn = sqlite3.connect('logindatabase.db')
+                        cursor = conn.cursor()
+                        cursor.execute('SELECT password FROM signup_info_prof WHERE username = ?', (session['username'],))
+                        existing_user = cursor.fetchone()
                 
-
-                return redirect(url_for('mainpage_prof'))
+                        if existing_user: 
+                                conn.commit()
+                                conn.close()
+                                flash('이미 존재하는 사용자 이름입니다.')
+                                return redirect(url_for('login'))
+                        
+                        else:
+                                conn.commit()
+                                conn.close()
+                                flash('존재하지 않는 계정입니다.')
+                                return render_template("login.html")
         else:
                 return render_template("loginpage.html")
-
-
-
-
+                
 
 @app.route('/mainpage_prof')
 def mainpage_prof():        
